77e13b2d2121ecda4cb632b96ddb21e7f3625ba8
Igor Leonovich <igorleo55@rambler.ru>
2022-08-07 20:59:03 +0300

    ДЗ3

---
+++ 3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+string Check(int num)
+{
+    string result;
+    if ((num % 2) == 0)
+    {
+        result = $"Число {num} чётное";
+    }
+    else
+    {
+        result = $"Число {num} нечётное";
+    }
+    return result;
+}
+int num = Prompt("Введите число >");
+System.Console.WriteLine(Check(num));
