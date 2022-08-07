ca322a74fe87a0470ecefb9de7696b14c7a10b5e
Igor Leonovich <igorleo55@rambler.ru>
2022-08-07 20:32:18 +0300

    ДЗ1

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+string CompareNumbers(int firstNum, int secondNum)
+{
+    string result;
+    if (firstNum > secondNum)
+    {
+        result = $"Число {firstNum} больше чем {secondNum}";
+    }
+    else
+    {
+        result = $"Число {secondNum} больше чем {firstNum}";
+    }
+    if (firstNum == secondNum)
+    {
+        result = $"Число {firstNum} равно {secondNum}";
+    }
+    return result;
+}
+
+int firstNum = Prompt("Введите 1е число >");
+int secondNum = Prompt("Введите 2е число >");
+System.Console.WriteLine(CompareNumbers(firstNum, secondNum));
