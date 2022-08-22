2110fb25e33be2f88c12e8d2c98a8c0e3ec2e5ea
Igor Leonovich <igorleo55@rambler.ru>
2022-08-22 20:04:51 +0300

    ДЗ4.2

---
+++ 4.1/4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+int Sum(int number)
+{   
+    int result = 0;
+    while (number > 0)
+    {
+        int a = number % 10;
+        result += a;
+        number = number / 10;
+    };
+    return result;
+}
+int number = Prompt("Введите число >");
+int sumNumber = Sum(number);
+System.Console.Write(sumNumber);
