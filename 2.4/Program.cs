483f1a676c05f24066f4541991aeecfa2b822102
Igor Leonovich <igorleo55@rambler.ru>
2022-08-27 19:02:26 +0300

    ДЗ 2.4

---
+++ 2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+string Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int num = int.Parse(value);
+    string result = "Такой день отсутствует";
+    if (num > 0 && num < 8)
+    {
+        result = "Рабочий день";
+
+        if (num == 6 || num == 7)
+        {
+            result = "Выходной";
+        }
+    }
+    return result;
+}
+string message = Prompt("Введите номер дня ");
+System.Console.Write(message);
