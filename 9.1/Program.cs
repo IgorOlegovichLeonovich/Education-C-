092abb5272d29416b54766a0fbc6c939d26c1b08
Igor Leonovich <igorleo55@rambler.ru>
2022-09-08 18:51:41 +0300

    ДЗ9.1

---
+++ 9.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+void FindPositiveNums(int m, int n)
+{
+    if (m > n) return;
+    if (m % 2 == 0) System.Console.Write($"{m} ");
+    FindPositiveNums(m + 1, n);
+}
+FindPositiveNums(Prompt("Введите первое число > "), Prompt("Введите второе число > "));
